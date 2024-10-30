a57b005d0fc24958ceab1ca5479618241e6acd72
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2024-10-30 16:05:48 -0300

    segurança com next-auth

--- src/app/privado/categoria/page.jsx
+++ src/app/privado/categoria/page.jsx
@@ -6,6 +6,8 @@ import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import Loading from '@/componentes/comuns/Loading';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth/auth";
 
 const deleteCategoria = async (codigo) => {
     'use server';
@@ -23,48 +25,57 @@ const deleteCategoria = async (codigo) => {
 export const dynamic = 'force-dynamic';
 
 export default async function Categoria() {
-  
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect("/api/auth/signin");
+    }
+
     const categorias = await getCategoriasDB()
 
     return (
-<Suspense fallback={<Loading />}>
-        <div style={{ padding: '20px' }}>
-            <h1>Categorias</h1>
-            <Link className="btn btn-primary"
-                href={`/privado/categoria/${0}/formulario`}>
-                Novo <i className="bi bi-file-earmark-plus"></i>
-            </Link>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th style={{
-                            textAlign: 'center'
-                        }}>Ações</th>
-                        <th>Código</th>
-                        <th>Nome</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {categorias.map((categoria) => (
-                        <tr key={categoria.codigo}>
-                            <td align="center">
-                                <Link className="btn btn-info"
-                                    href={`/privado/categoria/${categoria.codigo}/formulario`}>
-                                    <i className="bi bi-pencil-square"></i>
-                                </Link>
-                                <form action={deleteCategoria.bind(null, categoria.codigo)} className="d-inline">
-                                    <Button variant="danger" type='submit'>
-                                        <i className="bi bi-trash"></i>
-                                    </Button>
-                                </form>
-                            </td>
-                            <td>{categoria.codigo}</td>
-                            <td>{categoria.nome}</td>
+        <Suspense fallback={<Loading />}>
+            <div style={{ padding: '20px' }}>
+                <h1>Categorias</h1>
+                <Link className="btn btn-primary"
+                    href={`/privado/categoria/${0}/formulario`}>
+                    Novo <i className="bi bi-file-earmark-plus"></i>
+                </Link>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th style={{
+                                textAlign: 'center'
+                            }}>Ações</th>
+                            <th>Código</th>
+                            <th>Nome</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {categorias.map((categoria) => (
+                            <tr key={categoria.codigo}>
+                                <td align="center">
+                                    <Link className="btn btn-info"
+                                        href={`/privado/categoria/${categoria.codigo}/formulario`}>
+                                        <i className="bi bi-pencil-square"></i>
+                                    </Link>
+                                    {
+                                        session?.user?.tipo === 'A' &&
+                                        <form action={deleteCategoria.bind(null, categoria.codigo)} className="d-inline">
+                                            <Button variant="danger" type='submit'>
+                                                <i className="bi bi-trash"></i>
+                                            </Button>
+                                        </form>
+                                    }
+                                </td>
+                                <td>{categoria.codigo}</td>
+                                <td>{categoria.nome}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
         </Suspense>
     )
 }
--- src/app/privado/produto/page.jsx
+++ src/app/privado/produto/page.jsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 import Loading from '@/componentes/comuns/Loading';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth/auth";
 
 const deleteProduto = async (codigo) => {
     'use server';
@@ -21,6 +23,12 @@ const deleteProduto = async (codigo) => {
 export const dynamic = 'force-dynamic';
 
 export default async function Produto() {
+
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        redirect("/api/auth/signin");
+    }
 
     const produtos = await getProdutosDB();
 
@@ -55,11 +63,14 @@ export default async function Produto() {
                                         href={`/privado/produto/${produto.codigo}/formulario`}>
                                         <i className="bi bi-pencil-square"></i>
                                     </Link>
-                                    <form action={deleteProduto.bind(null, produto.codigo)} className="d-inline">
-                                        <Button variant="danger" type='submit'>
-                                            <i className="bi bi-trash"></i>
-                                        </Button>
-                                    </form>
+                                    {
+                                        session?.user?.tipo === 'A' &&
+                                        <form action={deleteProduto.bind(null, produto.codigo)} className="d-inline">
+                                            <Button variant="danger" type='submit'>
+                                                <i className="bi bi-trash"></i>
+                                            </Button>
+                                        </form>
+                                    }
                                 </td>
                                 <td>{produto.codigo}</td>
                                 <td>{produto.nome}</td>
